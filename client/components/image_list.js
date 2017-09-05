a76c1751c04ec39b1f30ae39852bd7ac59894e4e
Eduardo Samrsla <ed.zluhan@gmail.com>
2017-09-05 14:43:14 -0300

    nest ImageDetails component and add dummy data for testing

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ImageDetails from './image_details';
+
+const IMAGES = [
+    { title: 'Pen', link: 'https://www.placecage.com/c/600/400' },
+    { title: 'Pine Tree', link: 'https://www.placecage.com/600/400' },
+    { title: 'Mug', link: 'https://www.placecage.com/g/600/400' }
+];
 
 const ImageList = () => {
+    const RenderedImages = IMAGES.map(function(image) {
+        return <ImageDetails />
+    });
+
     return (
         <ul>
-            <li>Image Detail</li>
+            { RenderedImages }
         </ul>
     )
 };
